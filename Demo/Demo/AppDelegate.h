74c605f2b1e565fa8ee753e51950329a29b92191
王二东 <wangerdong@treefinance.com.cn>
2016-01-15 16:33:33 +0800

    Add demo

---
+++ Demo/Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by 王晓东 on 16/1/15.
+//  Copyright © 2016年 Ericdong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
